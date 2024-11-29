c2d4f5b2f9ad76568cb89a0523a88ac878ac151b
Mike Turner <turner.mike@gmail.com>
2024-11-29 16:28:29 -0500

    removed old files

--- src/components/Portfolio/Portfolio.js
+++ src/components/Portfolio/Portfolio.js
@@ -1,13 +1,6 @@
 import React from "react";
 import { Container, Row, Col, Nav, Tab } from "react-bootstrap";
-// import Button from "react-bootstrap/Button";
-// import { Tab, Tabs, TabList, TabPanel } from 'react-tabs';
 import 'react-tabs/style/react-tabs.css';
-// import pdf from "../../assets/pdf/MichaelTurner-Developer-Resume.pdf";
-// import { AiOutlineDownload } from "react-icons/ai";
-// import { Document, Page, pdfjs } from "react-pdf";
-// import "react-pdf/dist/esm/Page/AnnotationLayer.css";
-// pdfjs.GlobalWorkerOptions.workerSrc = `//cdnjs.cloudflare.com/ajax/libs/pdf.js/${pdfjs.version}/pdf.worker.min.js`;
 
 function Portfolio() {
 
@@ -46,38 +39,8 @@ function Portfolio() {
 
     </Container>    
     
-    // <div>
-      
-    //   <Container fluid className="portfolio-section">
-        
-    //     <Container className="portfolio-content">
-    //       <Row>
-            
-    //         <h1>Mike's Most Recent Work</h1>
-            
-    //         <Col md={3} className="list-wrapper" style={{ background: 'lightblue' }}>
-    //           <h2>Surrey Place</h2>
-    //           <ul>
-    //             <li>
-    //               <a href="#stripe-payments">Stripe Payments</a>
-    //             </li>
-    //             <li>
-    //               <a href="#camp-filters">Camp/Program Filters</a>
-    //             </li>                
-    //           </ul>
-    //         </Col>
-              
-    //         <Col md={9} className="feature-wrapper" style={{ background: 'lightgreen' }}>
-    //           <p>feature</p>
-    //         </Col>
-              
-    //       </Row>        
-    //     </Container>
-
-    //   </Container>
-      
-    // </div>
   );
+  
 }
 
 export default Portfolio;
