051b3a64e5526b4c247b5536e942aff7ed7c4a89
Mike Turner <turner.mike@gmail.com>
2024-11-30 12:39:15 -0500

    moved sp logo to file

--- src/components/Portfolio/Portfolio.js
+++ src/components/Portfolio/Portfolio.js
@@ -1,46 +1,74 @@
 import React from "react";
 import { Container, Row, Col, Nav, Tab } from "react-bootstrap";
 import 'react-tabs/style/react-tabs.css';
+import SurreyPlaceLogo from "../svg/SurreyPlaceLogo";
 
 function Portfolio() {
 
   return (
-    
 
-    <Container className="portfolio-section"> 
+    <Container className="portfolio-section">
 
       <Row>
         <h1>Mike's Portfolio</h1>
-        <p>Here's some project work that I've completed with some details about the task.</p>
-        
-        <h2>Surrey Place</h2>
-        <Tab.Container id="left-tabs-example" defaultActiveKey="first">
-          <Row>
-            <Col sm={3}>
-              <Nav variant="pills" className="flex-column">
-                <Nav.Item>
-                  <Nav.Link eventKey="first">Tab 1</Nav.Link>
-                </Nav.Item>
-                <Nav.Item>
-                  <Nav.Link eventKey="second">Tab 2</Nav.Link>
-                </Nav.Item>
-              </Nav>
-            </Col>
-            <Col sm={9}>
-              <Tab.Content>
-                <Tab.Pane eventKey="first">First tab content</Tab.Pane>
-                <Tab.Pane eventKey="second">Second tab content</Tab.Pane>
-              </Tab.Content>
-            </Col>
-          </Row>
-        </Tab.Container>
-                
+        <p>Here's an overview of my completed projects, along with key details about each task.</p>
       </Row>
 
-    </Container>    
-    
+      <Row>
+
+        {/* <h2>Surrey Place</h2> */}
+        <Tab.Container id="sp-tabs" defaultActiveKey="first">
+          <Col sm={3}>
+            <Nav className="flex-column">
+              <Nav.Item>
+                <SurreyPlaceLogo />
+              </Nav.Item>
+              <Nav.Item>
+                <Nav.Link eventKey="first">Stripe Payments with Custom Checkout</Nav.Link>
+              </Nav.Item>
+              <Nav.Item>
+                <Nav.Link eventKey="second">Camp/Program Filters</Nav.Link>
+              </Nav.Item>
+            </Nav>
+          </Col>
+          <Col sm={9}>
+            <Tab.Content>
+              <Tab.Pane eventKey="first">
+                <h3>Custom WordPress Plugin for Online Payments</h3>
+                <p>Developed a WordPress plugin integrating the Stripe API to facilitate secure online payments. The solution included a dynamic questionnaire form and flexible payment options, such as full payment, a 50/50 split, or a 30/40/30 subscription model tailored to program requirements. Additionally, implemented and documented a robust Git-based workflow, streamlining collaboration and continuous deployment using Bitbucket and WP Engine.</p>
+              </Tab.Pane>
+              <Tab.Pane eventKey="second">Second tab content</Tab.Pane>
+            </Tab.Content>
+          </Col>
+        </Tab.Container>
+      </Row>
+
+      <Row>
+        <h2>SiriusXM Canada</h2>
+        <Tab.Container id="sxm-tabs" defaultActiveKey="first">
+          <Col sm={3}>
+            <Nav className="flex-column">
+              <Nav.Item>
+                <Nav.Link eventKey="first">Tab 1</Nav.Link>
+              </Nav.Item>
+              <Nav.Item>
+                <Nav.Link eventKey="second">Tab 2</Nav.Link>
+              </Nav.Item>
+            </Nav>
+          </Col>
+          <Col sm={9}>
+            <Tab.Content>
+              <Tab.Pane eventKey="first">First tab content</Tab.Pane>
+              <Tab.Pane eventKey="second">Second tab content</Tab.Pane>
+            </Tab.Content>
+          </Col>
+        </Tab.Container>
+      </Row>
+
+    </Container>
+
   );
-  
+
 }
 
 export default Portfolio;
